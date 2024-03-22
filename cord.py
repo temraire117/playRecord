38d3ed408cdbd0d0e1824a1874ff9211a2442b97
temraire117 <temraire117@gmail.com>
2024-03-22 18:33:38 +0900

    id, 패스워드 입력

--- cord.py
+++ cord.py
@@ -1,1 +1,9 @@
 
+import requests
+from bs4 import BeautifulSoup
+
+pageLink = '<https://play.google.com/store/account/orderhistory>'
+userName = input()
+passWord = input()
+print(f"{userName}")
+print(f"{passWord}")
